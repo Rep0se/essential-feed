4bcaef55f13515690da47202aecada534bfbf237
Alexander Sundiev <asundiev@prometheusgroup.com>
2023-02-02 14:22:45 -0500

    Extract UIImage test helper to a separate file

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -435,31 +435,3 @@ private extension UIRefreshControl {
         })
     }
 }
-
-private extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img!
-    }
-    
-    func removeAlpha() -> UIImage {
-        let inputImage = self
-        let format = UIGraphicsImageRendererFormat.init()
-        format.opaque = true //Removes Alpha Channel
-        format.scale = inputImage.scale //Keeps original image scale.
-        let size = inputImage.size
-        return UIGraphicsImageRenderer(size: size, format: format).image { _ in
-            inputImage.draw(in: CGRect(origin: .zero, size: size))
-        }
-    }
-    
-    func pngData(withAlpha: Bool) -> Data? {
-        return withAlpha ? self.pngData() : self.removeAlpha().pngData()
-    }
-}
