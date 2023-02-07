e52cc47f29acc9966281ba84c92e1cb59fb520da
Alexander Sundiev <asundiev@prometheusgroup.com>
2023-02-07 14:20:42 -0500

    Remove unused FeedViewModel (it was replaced by the new `FeedPresenter`). Add Presentable View Models as pure data to clarify communication between Presentation and UI.

--- EssentialFeediOS/Feed Presentation/FeedViewModel.swift
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -5,29 +5,29 @@
 //  Created by Alexander Sundiev on 2/7/23.
 //
 
-import UIKit
-import EssentialFeed
-
-final class FeedViewModel {
-    typealias Observer<T> = (T) -> Void
-    
-    private let feedLoader: FeedLoader
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    var onLoadingStateChange: Observer<Bool>?
-    var onFeedLoad: Observer<[FeedImage]>?
-    
-    func loadFeed() {
-        onLoadingStateChange?(true)
-        feedLoader.load { [weak self] result in
-            guard let self = self else { return }
-            if let feed = try? result.get() {
-                self.onFeedLoad?(feed)
-            }
-            self.onLoadingStateChange?(false)
-        }
-    }
-}
+//import UIKit
+//import EssentialFeed
+//
+//final class FeedViewModel {
+//    typealias Observer<T> = (T) -> Void
+//
+//    private let feedLoader: FeedLoader
+//
+//    init(feedLoader: FeedLoader) {
+//        self.feedLoader = feedLoader
+//    }
+//
+//    var onLoadingStateChange: Observer<Bool>?
+//    var onFeedLoad: Observer<[FeedImage]>?
+//
+//    func loadFeed() {
+//        onLoadingStateChange?(true)
+//        feedLoader.load { [weak self] result in
+//            guard let self = self else { return }
+//            if let feed = try? result.get() {
+//                self.onFeedLoad?(feed)
+//            }
+//            self.onLoadingStateChange?(false)
+//        }
+//    }
+//}
