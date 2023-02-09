03d059b6bfc7c7e29523ff2ba1dbc8d41739d5f6
Alexander Sundiev <asundiev@prometheusgroup.com>
2023-02-09 16:36:49 -0500

    Extract FeedErrorViewModel, FeedLoadingViewModel, FeedViewModel into their separate files and ViewModels folder

---
+++ EssentialFeediOS/Feed Presentation/ViewModels/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alexander Sundiev on 2/9/23.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/ViewModels/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alexander Sundiev on 2/9/23.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
