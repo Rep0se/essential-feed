9bd6d93be08b5d7dd2e82d6f13d3ef5b75e00223
Alexander Sundiev <asundiev@prometheusgroup.com>
2023-02-13 15:53:59 -0500

    Move FeedImagePresenter and collaborators to production

--- EssentialFeed/Feed Presentation/ViewModels/FeedImageViewModel.swift
+++ EssentialFeed/Feed Presentation/ViewModels/FeedImageViewModel.swift
@@ -5,14 +5,14 @@
 //  Created by Alexander Sundiev on 2/9/23.
 //
 
-struct FeedImageViewModel<Image> {
-    let description: String?
-    let location: String?
-    let image: Image?
-    let isLoading: Bool
-    let shouldRetry: Bool
-    
-    var hasLocation: Bool {
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+
+    public var hasLocation: Bool {
         return location != nil
     }
 }
