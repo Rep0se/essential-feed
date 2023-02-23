2de83c8bfbad9d0ef725b0e042022be853da17f3
Alexander Sundiev <asundiev@prometheusgroup.com>
2023-02-23 16:45:26 -0500

    Duplicate RemoteFeedLoader as RemoteImageCommentsLoader

---
+++ EssentialFeed/Feed API/ImageCommentsMapper.swift
@@ -0,0 +1,18 @@
+//	
+// Copyright © 2023 Alexander Sundiev. All rights reserved.
+//
+
+import Foundation
+
+final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+        return root.items
+    }
+}
