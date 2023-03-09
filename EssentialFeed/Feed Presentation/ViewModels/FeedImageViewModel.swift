7f09fe00a78546418555719871ab235c0ea177c5
Alexander Sundiev <asundiev@prometheusgroup.com>
2023-03-09 17:47:19 -0500

    Remove unused FeedImagePresenter logic

--- EssentialFeed/Feed Presentation/ViewModels/FeedImageViewModel.swift
+++ EssentialFeed/Feed Presentation/ViewModels/FeedImageViewModel.swift
@@ -5,12 +5,9 @@
 //  Created by Alexander Sundiev on 2/9/23.
 //
 
-public struct FeedImageViewModel<Image> {
+public struct FeedImageViewModel {
     public let description: String?
     public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
 
     public var hasLocation: Bool {
         return location != nil
