434e28661640a53f4bc8836a3f8e2b20b07e56c9
Alexander Sundiev <asundiev@prometheusgroup.com>
2023-03-14 15:03:14 -0400

    Extract shared ListViewController snapshot tests

---
+++ EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -0,0 +1,44 @@
+//	
+// Copyright © 2023 Alexander Sundiev. All rights reserved.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+final class ListSnapshotTests: XCTestCase {
+    
+    func test_emptyList() {
+        let sut = makeSUT()
+        
+        sut.display(emptyList())
+        
+        assert(snapshot: sut.snapshot(for: .iPhoneSE3rdGen(style: .light)), named: "EMPTY_LIST_light")
+        assert(snapshot: sut.snapshot(for: .iPhoneSE3rdGen(style: .dark)), named: "EMPTY_LIST_dark")
+    }
+
+    func test_listWithErrorMessage() {
+        let sut = makeSUT()
+        
+        sut.display(.error(message: "This is\na multi-line\nerror message"))
+        
+        assert(snapshot: sut.snapshot(for: .iPhoneSE3rdGen(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhoneSE3rdGen(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func emptyList() -> [CellController] {
+        return []
+    }
+}
