e7927cf138da649b8d366c4f2a2bc691d18ba317
Alexander Sundiev <asundiev@prometheusgroup.com>
2023-03-15 10:04:21 -0400

    Implement Image Comments UI

---
+++ EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,30 @@
+//	
+// Copyright © 2023 Alexander Sundiev. All rights reserved.
+//
+
+import UIKit
+import EssentialFeed
+
+public class ImageCommentCellController: CellController {
+    private let model: ImageCommentViewModel
+    
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+    
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.usernameLabel.text = model.username
+        cell.dateLabel.text = model.date
+        cell.messageLabel.text = model.message
+        return cell
+    }
+    
+    public func preload() {
+        
+    }
+    
+    public func cancelLoad() {
+        
+    }
+}
---
+++ EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,11 @@
+//	
+// Copyright © 2023 Alexander Sundiev. All rights reserved.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+    @IBOutlet private(set) public var messageLabel: UILabel!
+}
