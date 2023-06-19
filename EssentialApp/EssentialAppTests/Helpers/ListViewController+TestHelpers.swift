31055748a216a73d35b08762d6101afc6e7f5961
Alexander Sundiev <asundiev@prometheusgroup.com>
2023-06-19 17:14:46 -0400

    Render comments

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -19,18 +19,51 @@ extension ListViewController {
         return refreshControl?.isRefreshing == true
     }
     
-    func simulateErrorViewTap() {
-        errorView.simulateTap()
-    }
-    
     var errorMessage: String? {
         return errorView.message
+    }
+    
+    func simulateErrorViewTap() {
+        errorView.simulateTap()
     }
     
     func simulateUserInitiatedReload() {
         refreshControl?.simulatePullToRefresh()
     }
+}
+
+// Comments
+extension ListViewController {
+    func numberOfRenderedComments() -> Int {
+        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: commentsSection)
+    }
     
+    func commentMessage(at row: Int) -> String? {
+        commentView(at: row)?.messageLabel.text
+    }
+    
+    func commentDate(at row: Int) -> String? {
+        commentView(at: row)?.dateLabel.text
+    }
+    
+    func commentUsername(at row: Int) -> String? {
+        commentView(at: row)?.usernameLabel.text
+    }
+    
+    func commentView(at row: Int) -> ImageCommentCell? {
+        guard numberOfRenderedComments() > row else { return nil }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: commentsSection)
+        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+    }
+    
+    private var commentsSection: Int {
+        return 0
+    }
+}
+
+// Images
+extension ListViewController {
     @discardableResult
     func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
         return feedImageView(at: index) as? FeedImageCell
